a5686df2a548165dc6fde1e0cc60d828c273213b
Carter <andrewscart@gmail.com>
2020-03-30 02:30:39 -0700

    gameplay prototype

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    bool vulnerable=false;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnMouseDown()
+    {
+        print("attack attempt");
+        if (vulnerable)
+            Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.forward * speed;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        print("entered zone");
+        vulnerable = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        print("exited zone");
+        vulnerable = false;
+    }
+}
---
+++ Assets/Scripts/TestEnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator spawn()
+    {
+        while (true) {
+            var spawn=Instantiate(enemyPrefab, transform);
+            spawn.transform.position += spawn.transform.right * Random.Range(0, 5);
+            yield return new WaitForSeconds(spawnRate);
+
+        }
+    }
+
+}
