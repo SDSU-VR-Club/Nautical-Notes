549b2a80599f49e501fc34e084e48b074c0fbf68
Roselynn Conrady <conrady.roselynn@gmail.com>
2020-04-03 18:16:11 -0700

    working basic cartoon water shader

--- Assets/Scripts/WaterScript.cs
+++ Assets/Scripts/WaterScript.cs
@@ -1,17 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class WaterScript : MonoBehaviour
 {
     public int materialIndex = 0;
-    public Vector2 uvAnimationRate = new Vector2(1.0f, 1.0f);
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
     public string textureName = "_MainTex";
 
     Vector2 uvOffset = Vector2.zero;
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         uvOffset += (uvAnimationRate * Time.deltaTime);
         if (GetComponent<Renderer>().enabled)
