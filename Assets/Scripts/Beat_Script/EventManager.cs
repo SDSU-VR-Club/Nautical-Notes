1c1ea5db7e91c5e39e1582712a7efdbea465758a
CrispyBoardgames <44710114+CrispyBoardgames@users.noreply.github.com>
2020-04-17 12:33:37 -0700

    Beat Spawning
    Added beat spawning with the RhythmTool.
    Works as expected. Spawning could be tweaked to be smarter by analyzing drastic changes in the song.
    
    Whole folder and code needs cleaning.

---
+++ Assets/Scripts/Beat_Script/EventManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RhythmTool;
+
+public class EventManager : MonoBehaviour
+{
+    public RhythmAnalyzer analyzer;
+    public RhythmPlayer player;
+    public RhythmEventProvider eventProvider;
+
+    public GameObject enemyPrefab;
+    public Transform[] spawnPositions;
+    public Transform EnemyHolder;
+
+    void Awake()
+    {
+        analyzer.Initialized += OnInitialized;
+        eventProvider.Register<Beat>(OnBeat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnInitialized(RhythmData rhythmData)
+    {
+        //Start playing the song.
+        player.Play();
+    }
+    //OnBeat, call spawner.
+    private void OnBeat(Beat beat)
+    {
+        var spawn = Instantiate(enemyPrefab, EnemyHolder);
+        int plank = Random.RandomRange(0, 3);
+        spawn.transform.position = spawnPositions[plank].position;
+        spawn.transform.forward = spawnPositions[plank].forward;
+    }
+}
