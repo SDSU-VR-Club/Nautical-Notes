9bdc895db99a963a78f04982c53893ce005b3b36
abigaildabu <44449631+abigaildabu@users.noreply.github.com>
2020-04-24 12:04:42 -0700

    Implementing speed
    Created method to change enemy's speed based on BPM

--- Assets/Scripts/Beat_Script/EventManager.cs
+++ Assets/Scripts/Beat_Script/EventManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RhythmTool;
+//using Mathf;
 
 public class EventManager : MonoBehaviour
 {
@@ -14,12 +15,18 @@ public class EventManager : MonoBehaviour
     public Transform[] spawnPositions;
     public Transform EnemyHolder;
 
+    public Transform Zone;
+
+    private Enemy en;
+
+    float currentBPM = 0.0f;
+
     void Awake()
     {
         analyzer.Initialized += OnInitialized;
         eventProvider.Register<Beat>(OnBeat);
-        Enemy en = enemyPrefab.GetComponent<Enemy>();
-        en.speed = 5.0f;
+        en = enemyPrefab.GetComponent<Enemy>();
+        
     }
 
     // Update is called once per frame
@@ -37,6 +44,12 @@ public class EventManager : MonoBehaviour
     //OnBeat, call spawner.
     private void OnBeat(Beat beat)
     {
+        // If statement for if the BPM changes
+        if ((currentBPM - beat.bpm) >= 1 || (currentBPM - beat.bpm) <= -1 )
+        {
+            en.speed = ChangeSpeed(beat);
+        }
+
         var rand = new System.Random();
         if(rand.Next()%2 == 1)
         {
@@ -44,6 +57,17 @@ public class EventManager : MonoBehaviour
             int plank = UnityEngine.Random.RandomRange(0, 3);
             spawn.transform.position = spawnPositions[plank].position;
             spawn.transform.forward = spawnPositions[plank].forward;
-        }        
+        }
+
+        currentBPM = beat.bpm;
+    }
+
+    private float ChangeSpeed(Beat beat)
+    {
+        // Duration of beat: t = (1/BPM) * 60 (resulting in seconds)
+        // Speed: S = d/t, d = (Zspawn) - (Zzone) / duration of beat
+        float beatduration = (1 / beat.bpm) * 60;
+        float speed = (Mathf.Abs((spawnPositions[0].transform.position.z) - (Zone.transform.position.z))) / beatduration;
+        return speed;
     }
 }
