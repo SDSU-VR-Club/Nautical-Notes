08820947e5b1fe28a352dd91c474e5aab01e3683
Roselynn Conrady <conrady.roselynn@gmail.com>
2020-04-29 15:27:13 -0700

    Reimplemented Main Menu and tilted the camera in TrippleGhostScene

---
+++ Assets/Scripts/MainMenu01.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenu01 : MonoBehaviour
+{
+
+    public GameObject PlayGameButton;
+    public GameObject HowToPlayButton;
+    public GameObject QuitGameButton;
+    public GameObject CreditsButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // make sure the TripplGhostScene is set as scene 1 in Build Settings
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
