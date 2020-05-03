6ea0904e03be9b7843d2a3c8a0c441acdb18983a
Roselynn Conrady <conrady.roselynn@gmail.com>
2020-05-02 17:40:05 -0700

    Credits button works

--- Assets/Scripts/MainMenu01.cs
+++ Assets/Scripts/MainMenu01.cs
@@ -11,18 +11,19 @@ public class MainMenu01 : MonoBehaviour
     public GameObject PlayGameButton;
     public GameObject HowToPlayButton;
     public GameObject QuitGameButton;
-    public GameObject CreditsButton;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject eventSystem;
+    bool creditsOpen;
+    public GameObject backButtonCredits;
+    public GameObject credits;
+    public GameObject creditsCanvas;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Back") && creditsOpen)
+        {
+            toggleCredits();
+        }
     }
 
     public void PlayGame ()
@@ -30,7 +31,25 @@ public class MainMenu01 : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         // make sure the TripplGhostScene is set as scene 1 in Build Settings
     }
-
+    public void ViewCredits ()
+    {
+        toggleCredits();
+    }
+    void toggleCredits()
+    {
+        if(creditsOpen)
+        {
+            creditsCanvas.SetActive(false);
+            eventSystem.GetComponent<EventSystem>().SetSelectedGameObject(credits);
+            creditsOpen = false;
+        }
+        else
+        {
+            creditsCanvas.SetActive(true);
+            eventSystem.GetComponent<EventSystem>().SetSelectedGameObject(backButtonCredits);
+            creditsOpen = true;
+        }
+    }
     public void QuitGame()
     {
         Application.Quit();
