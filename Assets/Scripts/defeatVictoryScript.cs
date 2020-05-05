0403ba85c6c3334081e8750b3b28606cfcd22f0e
Roselynn Conrady <conrady.roselynn@gmail.com>
2020-05-04 20:22:44 -0700

    "try again", "main menu", and "quit" buttons in defeat scene

---
+++ Assets/Scripts/defeatVictoryScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class defeatVictoryScript : MonoBehaviour
+{
+    public GameObject TryAgainButton;
+    public GameObject MainMenuButton;
+    public GameObject QuitGameButton;
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
